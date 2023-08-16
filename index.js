26d8858fd32f68177f3fe7d257005f4e4e8ffd72
MuhammadShakerAlKilany <120374233+MuhammadShakerAlKilany@users.noreply.github.com>
2023-08-16 14:32:15 +0000

    add mongoose

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const usersSchema = require("./schema") 
 const app = express()
-
+const mongoose = require('mongoose');
 app.get('/', function (req, res) {
   res.send('Hello World')
   
 })
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+  .then(() => console.log('Connected!'));
 app.listen(3000)
